f5f182a6236f88dd78238a4b02c9920d504931d3
Stephen Giang <stepheng753@gmail.com>
2021-10-12 02:41:12 -0700

    cleaned up and commented

--- LeetCode/RainWater/Solution.py
+++ LeetCode/RainWater/Solution.py
@@ -7,8 +7,7 @@ class Solution:
         col_length = len(heightMap)
         index = 0
         orig_arr = heightMap.copy()
-        if type(heightMap[0][0]) is not dict:
-            heightMap = self.initHeightMap(orig_arr)
+        heightMap = self.initHeightMap(orig_arr)
 
         while (index < row_length * col_length):
             row_num = int(index / row_length)
@@ -20,27 +19,36 @@ class Solution:
                 left = heightMap[row_num][col_num - 1]
                 right = heightMap[row_num][col_num + 1]
             
+                # Finds the lowest wall
+                # A wall being either a border that does not change or a block that is already filled
+                # We always know that the top and left are either borders or water filled based on the indexing
+                # We always know that the bottom and right are either borders or not water filled based on indexing
                 lowest_wall = {'height': float('inf'), 'is_border': True, 'filledWithWater': False};
                 if top['height'] < lowest_wall['height']:
                     lowest_wall = top
-                if (bottom['isBorder'] or bottom['filledWithWater']) and bottom['height'] < lowest_wall['height']:
+                if bottom['isBorder'] and bottom['height'] < lowest_wall['height']:
                     lowest_wall = bottom
                 if left['height'] < lowest_wall['height']:
                     lowest_wall = left
-                if (right['isBorder'] or right['filledWithWater']) and right['height'] < lowest_wall['height']:
+                if right['isBorder'] and right['height'] < lowest_wall['height']:
                     lowest_wall = right
 
+                # Matches the water level to meet the height of lowest wall
+                # The maximum amount before overflowing
                 if heightMap[row_num][col_num]['height'] < lowest_wall['height']:
                     heightMap[row_num][col_num]['height'] = lowest_wall['height']
                     heightMap[row_num][col_num]['filledWithWater'] = True
 
+                # Checks to see if I need to adjust the rest of the blocks
                 heightMap = self.recursiveCheck(orig_arr, heightMap, index, row_length)
-
                 
             index = index + 1
 
+        # The difference of the two arrays will be amount of water added per block, and the sum will be the total amount of water added
         return self.sum2dArr(self.subtractElementWise2dArr(heightMap, orig_arr))
         
+
+    # This takes the sum of all the elements in a 2d arr
     def sum2dArr(self, arr):
         sum = 0
         for row in arr:
@@ -48,6 +56,8 @@ class Solution:
                 sum = sum + element
         return sum
 
+
+    # This subtracts 2 2d Arrays element wise
     def subtractElementWise2dArr(self, heightMap, orig_arr):
         arr2d = []
         for i in range(0, len(heightMap)):
@@ -58,7 +68,8 @@ class Solution:
         return arr2d
 
 
-
+    # Allows for me to understand the following info on a box,
+    # The boxes height, whether or not it is a border, and whether or not it is filled with water
     def initHeightMap(self, heightMap):
         arr2d = []
         for i in range(0, len(heightMap)):
@@ -70,6 +81,10 @@ class Solution:
             arr2d.append(arr1d)
         return arr2d
         
+
+    # Recursively Checks to see the following
+    # If the adjacent boxes that are filled with water are higher than the current box, then the water will overflow
+    # So it either matches the water level to the height of the current or back to original size, whatever is biggest 
     def recursiveCheck(self, orig_arr, heightMap, index, row_length):
         row_num = int(index / row_length)
         col_num = int(index % row_length)
@@ -108,8 +123,7 @@ class Solution:
                 heightMap[row_num][col_num + 1]['height'] = orig_arr[row_num][col_num + 1]
             heightMap = self.recursiveCheck(orig_arr, heightMap, index + 1, row_length)
             
-        return heightMap
-            
+        return heightMap        
 
 
 def main():
