89a1c71c5009c584b0d7082b7131990bd1da0b9c
Stephen Giang <stepheng753@gmail.com>
2022-04-08 00:30:33 -0700

    Code is implemented, just cleaning up all of my terrible arrays into dictionaries still

--- Web Development/p5.js/ChessAI/board.js
+++ Web Development/p5.js/ChessAI/board.js
@@ -1,13 +1,20 @@
-let canvasSize = 900;
-let numRows = 8;
-let numCols = 8;
+const canvasSize = 900;
+const numRows = 8;
+const numCols = 8;
+const squareSize = canvasSize / numRows;
 let squares = new Array(64);
 let pieces = new Array(64);
 let img;
+let moveFromIndex = -1;
 let currLegalMoves = [];
-let originalIndex = -1;
-let isWhitesTurn = true;
 let moves = [];
+let isWhitesTurn = true;
+let promotionMode = false;
+const defaultSquareColor = '#A3524C';
+const selectionColor = '#e9eba2';
+const legalMoveColor = '#59b381';
+const captureColor = '#4d6cfa';
+const checkColor = '#c74343';
 
 function setup() {
 	createCanvas(canvasSize, canvasSize);
@@ -15,46 +22,15 @@ function setup() {
 	// setPieces('r7ppp3pp4k7n5P8K4PP5b0R2R3');
 }
 
-function draw() {
-	drawSquares();
-	drawAllPieces();
-}
-
-function drawAllPieces() {
-	for (let i = 0; i < pieces.length; i++) {
-		if (pieces[i] && pieces[i] != 0) {
-			pieces[i].drawPiece();
-		}
-	}
-}
-
-function drawSquares() {
-	for (let i = 0; i < squares.length; i++) {
-		let x = (i % numRows) * (canvasSize / numRows);
-		let y = Math.floor(i / numRows) * (canvasSize / numCols);
-		let width = canvasSize / numRows;
-		fill(squares[i]);
-		rect(x, y, width, width);
-	}
-}
-
-function initSquares() {
-	for (let i = 0; i < pieces.length; i++) {
-		if ((i % 2 == 1 && Math.floor(i / numRows) % 2 == 0) || (i % 2 == 0 && Math.floor(i / numRows) % 2 == 1)) {
-			squares[i] = '#A3524C';
-		} else {
-			squares[i] = 255;
-		}
-	}
-	checkForCheck(true);
-}
-
-function initEmptySpace() {
-	for (let i = 0; i < pieces.length; i++) {
-		if (!pieces[i]) {
-			pieces[i] = 0;
-		}
-	}
+function initChess() {
+	initPawns();
+	initBishops();
+	initKnights();
+	initRooks();
+	initQueens();
+	initKings();
+	initEmptySpace();
+	initSquares();
 }
 
 function initPawns() {
@@ -97,89 +73,189 @@ function initKings() {
 	pieces[60] = new King(60, true);
 }
 
-function initChess() {
-	initPawns();
-	initBishops();
-	initKnights();
-	initRooks();
-	initQueens();
-	initKings();
-	initEmptySpace();
-	initSquares();
+function initEmptySpace() {
+	for (let i = 0; i < pieces.length; i++) {
+		if (!pieces[i]) {
+			pieces[i] = 0;
+		}
+	}
+}
+
+function initSquares() {
+	for (let i = 0; i < pieces.length; i++) {
+		if ((i % 2 == 1 && Math.floor(i / numRows) % 2 == 0) || (i % 2 == 0 && Math.floor(i / numRows) % 2 == 1)) {
+			squares[i] = defaultSquareColor;
+		} else {
+			squares[i] = 255;
+		}
+	}
+	checkIfCurrentInCheck(true);
+}
+
+function draw() {
+	drawSquares();
+	drawAllPieces();
+
+	if (promotionMode) {
+		drawPromotionPicker();
+	}
+}
+
+function drawSquares() {
+	for (let i = 0; i < squares.length; i++) {
+		let x = convertIndexToPixel(i).x;
+		let y = convertIndexToPixel(i).y;
+		fill(squares[i]);
+		rect(x, y, squareSize, squareSize);
+	}
+}
+
+function drawAllPieces() {
+	for (let i = 0; i < pieces.length; i++) {
+		if (pieces[i] && pieces[i] != 0) {
+			pieces[i].drawPiece();
+		}
+	}
+}
+
+// Tester: setPieces('rnbqkbnrppppP00p00000000000000000000000000000000PPPPP0PPRNBQKBNR')
+function drawPromotionPicker() {
+	fill(100, 100, 100, 225);
+	rect(0, 0, canvasSize, canvasSize);
+	let choices = img.get(img.width / 6, isWhitesTurn ? img.height / 2 : 0, (4 / 6) * img.width, img.width / 6);
+	choices.resize(4 * squareSize, squareSize);
+	image(choices, 2 * squareSize, 3.5 * squareSize);
 }
 
 function mouseClicked() {
-	if (mouseX >= 0 && mouseX <= canvasSize && mouseY >= 0 && mouseY <= canvasSize) {
-		let index = convertPixelToIndex(mouseX, mouseY);
+	// If in promotion mode, let user choose promotion piece
+	if (promotionMode) {
+		promotionPicker();
+	}
+
+	// Check if user clicked inside canvas
+	else if (mouseX >= 0 && mouseX <= canvasSize && mouseY >= 0 && mouseY <= canvasSize) {
+		let currIndex = convertPixelToIndex(mouseX, mouseY);
+
 		if (
-			((originalIndex < 0 && pieces[index] != 0) || pieces[index] != 0) &&
-			pieces[index].isWhite == isWhitesTurn
+			((moveFromIndex < 0 && pieces[currIndex] != 0) || // If no piece selected previously, and piece currently selected is a Piece
+				pieces[currIndex] != 0) && // If selected a new piece
+			pieces[currIndex].isWhite == isWhitesTurn // If piece is of the correct color
 		) {
 			initSquares();
-			squares[index] = '#e9eba2';
+			squares[currIndex] = selectionColor;
 			currLegalMoves = pieces[convertPixelToIndex(mouseX, mouseY)].getLegalMoves(true, true);
-			originalIndex = index;
+			moveFromIndex = currIndex;
+
 			return;
 		}
-		let foundElement = findsArray2D(currLegalMoves, index);
-		if (foundElement[0] != -1) {
-			let temp = pieces[originalIndex];
-			temp.index = index;
-			pieces[originalIndex] = 0;
-			pieces[index] = temp;
-			pieces[index].numMoves++;
-			moves.push([pieces[index], originalIndex, index]);
 
-			// En Pessant
-			let indexingDirection = pieces[index].isWhite ? 1 : -1;
-			if (foundElement[1] == 'e.p.') {
-				pieces[index + indexingDirection * numRows] = 0;
-			}
+		// If FROM piece is selected, choose TO piece
+		let foundIndex = findsLegalMoves(currLegalMoves, currIndex);
+		if (foundIndex != -1) {
+			let foundElement = currLegalMoves[foundIndex];
+			// Change Piece selected to previous piece and update index, and leave previous piece empty
+			pieces[moveFromIndex].index = currIndex;
+			pieces[currIndex] = pieces[moveFromIndex];
+			pieces[currIndex].numMoves++;
+			pieces[moveFromIndex] = 0;
+			moves.push([pieces[currIndex], moveFromIndex, currIndex]);
 
-			// Castle
-			if (foundElement[1] == '0-0' || foundElement[1] == '0-0-0') {
-				if (index < originalIndex) {
-					pieces[index + 1] = pieces[index - (index % numRows)];
-					pieces[index + 1].numMoves++;
-					pieces[index + 1].index = index + 1;
-					moves.push([pieces[index + 1], index - (index % numRows), index + 1]);
-				} else if (index > originalIndex) {
-					pieces[index - 1] = pieces[index + (numRows - 1) - (index % numRows)];
-					pieces[index - 1].numMoves++;
-					pieces[index - 1].index = index - 1;
-					moves.push([pieces[index - 1], index + (numRows - 1) - (index % numRows), index - 1]);
+			// En Passant
+			// If en passant, remove the piece above/below new space
+			if (pieces[currIndex].constructor.name == 'Pawn') {
+				let behindDirection = -1 * pieces[currIndex].getIndexingDirection();
+				if (foundElement.type == 'e.p.') {
+					pieces[currIndex + indexingDirection * numRows] = 0;
 				}
 			}
 
-			isWhitesTurn = !isWhitesTurn;
-			initSquares();
-			squares[originalIndex] = '#e9eba2';
-
-			// Capture
-			if (foundElement[1] == 'x') {
-				squares[index] = '#4d6cfa';
-			} else {
-				squares[index] = '#e9eba2';
+			// Castle
+			if (foundElement.type == '0-0' || foundElement.type == '0-0-0') {
+				// Absolute Right Castling, move Rook
+				if (currIndex < moveFromIndex) {
+					pieces[currIndex + 1] = pieces[currIndex - getColNum(currIndex)];
+					pieces[currIndex + 1].numMoves++;
+					pieces[currIndex + 1].index = currIndex + 1;
+					moves.push([pieces[currIndex + 1], currIndex - getColNum(currIndex), currIndex + 1]);
+				}
+				// Absolute Left Castling, move Rook
+				else if (currIndex > moveFromIndex) {
+					pieces[currIndex - 1] = pieces[currIndex + (numRows - 1) - getColNum(currIndex)];
+					pieces[currIndex - 1].numMoves++;
+					pieces[currIndex - 1].index = currIndex - 1;
+					moves.push([
+						pieces[currIndex - 1],
+						currIndex + (numRows - 1) - getColNum(currIndex),
+						currIndex - 1,
+					]);
+				}
 			}
 
-			checkForCheck(true);
+			// Pawn Promotion
+			if (foundElement.type.includes('=')) {
+				promotionMode = true;
+			}
+
+			isWhitesTurn = !isWhitesTurn; // Switch turns
+			initSquares(); // Reset squares
+			squares[moveFromIndex] = selectionColor; // Highlighted FROM square
+
+			// Capture
+			if (foundElement.type == 'X') {
+				squares[currIndex] = captureColor;
+			} else {
+				squares[currIndex] = selectionColor;
+			}
+
+			// Check to see if this move, put opponent in check
+			checkIfCurrentInCheck(true);
 		} else {
 			initSquares();
 		}
-		originalIndex = -1;
+		moveFromIndex = -1;
 		currLegalMoves = [];
 	}
 }
 
-function checkForCheck(show) {
-	let kingIndex = getKingIndex();
+function promotionPicker() {
+	let y1 = 3.5 * squareSize;
+	let y2 = 4.5 * squareSize;
+	let promotionIndex = moves[moves.length - 1][2];
+
+	let promotionChoices = [
+		new Queen(promotionIndex, !isWhitesTurn),
+		new Bishop(promotionIndex, !isWhitesTurn),
+		new Knight(promotionIndex, !isWhitesTurn),
+		new Rook(promotionIndex, !isWhitesTurn),
+	];
+	for (let i = 2; i < 6; i++) {
+		let x1 = i * squareSize;
+		let x2 = x1 + squareSize;
+		if (mouseX >= x1 && mouseX <= x2 && mouseY >= y1 && mouseY <= y2) {
+			pieces[promotionIndex] = promotionChoices[i - 2];
+		}
+	}
+	promotionMode = false;
+	checkIfCurrentInCheck(true);
+}
+
+/**
+ * this function will check if the current player is in check
+ * @param {*} show
+ * @returns
+ */
+function checkIfCurrentInCheck(show) {
+	let kingIndex = pieces.findIndex(
+		(element) => element.isWhite == isWhitesTurn && element.constructor.name == 'King'
+	);
 	for (let i = 0; i < pieces.length; i++) {
 		if (pieces[i] != 0 && pieces[i].isWhite != isWhitesTurn) {
 			let legalMoves = pieces[i].getLegalMoves(false);
-			let foundElement = findsArray2D(legalMoves, kingIndex);
-			if (foundElement[0] != -1) {
+			let findIndex = findsLegalMoves(legalMoves, kingIndex);
+			if (findIndex != -1) {
 				if (show) {
-					squares[kingIndex] = '#c74343';
+					squares[kingIndex] = checkColor;
 				}
 				return true;
 			}
@@ -187,21 +263,45 @@ function checkForCheck(show) {
 	}
 }
 
-function getKingIndex() {
-	return pieces.findIndex((element) => element.isWhite == isWhitesTurn && element.constructor.name == 'King');
-}
-
+/**
+ * -------------------------------------------------
+ * HELPER FUNCTIONS
+ * -------------------------------------------------
+ */
 function findsArray2D(array2D, element) {
 	for (let i = 0; i < array2D.length; i++) {
 		if (array2D[i].includes(element)) {
-			return array2D[i];
+			return i;
 		}
 	}
-	return [-1];
+	return -1;
+}
+
+function findsLegalMoves(legalMoves, moveIndex) {
+	for (let i = 0; i < legalMoves.length; i++) {
+		if (legalMoves[i].moveIndex == moveIndex) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+function convertIndexToPixel(index) {
+	let x = getColNum(index) * squareSize;
+	let y = getRowNum(index) * squareSize;
+	return { x: x, y: y };
 }
 
 function convertPixelToIndex(x, y) {
-	return Math.floor(x / (canvasSize / numRows)) + Math.floor(y / (canvasSize / numCols)) * numRows;
+	return Math.floor(x / squareSize) + Math.floor(y / squareSize) * numRows;
+}
+
+function getRowNum(index) {
+	return Math.floor(index / numRows);
+}
+
+function getColNum(index) {
+	return index % numRows;
 }
 
 function setPieces(piecesString) {
@@ -221,15 +321,38 @@ function setPieces(piecesString) {
 			pieces[piecesIndex] = new Queen(piecesIndex++, pieceChar.toUpperCase() == pieceChar);
 		} else if (pieceChar.toLowerCase() == 'k') {
 			pieces[piecesIndex] = new King(piecesIndex++, pieceChar.toUpperCase() == pieceChar);
-		} else if (!isNaN(pieceChar) && pieceChar != '0') {
-			for (let k = 0; k < int(pieceChar); k++) {
-				pieces[piecesIndex++] = 0;
-			}
 		} else if (pieceChar.toLowerCase() == 'm') {
 			isWhitesTurn = false;
 		} else {
 			pieces[piecesIndex++] = 0;
 		}
 	}
-	checkForCheck(true);
+	checkIfCurrentInCheck(true);
+}
+
+function getPieces() {
+	let piecesString = '';
+	for (let i = 0; i < pieces.length; i++) {
+		if (pieces[i] != 0) {
+			if (pieces[i].constructor.name == 'Pawn') {
+				piecesString += pieces[i].isWhite ? 'P' : 'p';
+			} else if (pieces[i].constructor.name == 'Bishop') {
+				piecesString += pieces[i].isWhite ? 'B' : 'b';
+			} else if (pieces[i].constructor.name == 'Knight') {
+				piecesString += pieces[i].isWhite ? 'N' : 'n';
+			} else if (pieces[i].constructor.name == 'Rook') {
+				piecesString += pieces[i].isWhite ? 'R' : 'r';
+			} else if (pieces[i].constructor.name == 'Queen') {
+				piecesString += pieces[i].isWhite ? 'Q' : 'q';
+			} else if (pieces[i].constructor.name == 'King') {
+				piecesString += pieces[i].isWhite ? 'K' : 'k';
+			}
+		} else {
+			piecesString += '0';
+		}
+	}
+	if (!isWhitesTurn) {
+		piecesString += 'm';
+	}
+	return piecesString;
 }
--- Web Development/p5.js/ChessAI/piece.js
+++ Web Development/p5.js/ChessAI/piece.js
@@ -11,12 +11,12 @@ class Piece {
 
 		let y = this.isWhite ? 0 : img.height / 2;
 		this.piecePic = img.get((numPic * img.width) / 6, y, img.width / 6, img.width / 6);
-		this.piecePic.resize(canvasSize / numRows, canvasSize / numRows);
+		this.piecePic.resize(squareSize, squareSize);
 	}
 
 	drawPiece() {
-		let x = (this.index % numRows) * (canvasSize / numRows);
-		let y = Math.floor(this.index / numRows) * (canvasSize / numCols);
+		let x = convertIndexToPixel(this.index).x;
+		let y = convertIndexToPixel(this.index).y;
 		image(this.piecePic, x, y);
 	}
 
@@ -25,12 +25,13 @@ class Piece {
 		while (i < this.legalMoves.length) {
 			let currMove = this.legalMoves[i];
 			let ogTemp = pieces[this.index];
-			let newTemp = pieces[currMove[0]];
+			let newTemp = pieces[currMove.moveIndex];
 
 			pieces[this.index] = 0;
-			pieces[this.legalMoves[i][0]] = ogTemp;
+			pieces[this.legalMoves[i].moveIndex] = ogTemp;
 
-			if (checkForCheck(false)) {
+			// If move puts king in check, remove move from list
+			if (checkIfCurrentInCheck(false)) {
 				this.legalMoves = this.legalMoves.filter((value) => {
 					return value != currMove;
 				});
@@ -38,8 +39,14 @@ class Piece {
 			}
 
 			pieces[this.index] = ogTemp;
-			pieces[currMove[0]] = newTemp;
+			pieces[currMove.moveIndex] = newTemp;
 			i++;
+		}
+	}
+
+	showLegalMoves() {
+		for (let i = 0; i < this.legalMoves.length; i++) {
+			squares[this.legalMoves[i].moveIndex] = legalMoveColor;
 		}
 	}
 }
@@ -49,44 +56,51 @@ class Pawn extends Piece {
 		super(index, isWhite, 5);
 	}
 
-	getLegalMoves(showMoves, doCheck = false) {
+	getLegalMoves(showMoves, runFilterCheck = false) {
 		this.legalMoves = [];
+		let end = this.isWhite ? 0 : numRows - 1;
 
-		let numMovesForward =
-			Math.floor(this.index / numRows) == 1 || Math.floor(this.index / numRows) == numRows - 2 ? 2 : 1;
-		let indexingDirection = this.isWhite ? -1 : 1;
+		// If pawn is on starting row, add 2 moves
+		let numMovesForward = this.numMoves == 0 ? 2 : 1;
+		let indexingDirection = this.getIndexingDirection();
 		for (let i = 1; i <= numMovesForward; i++) {
 			let possibleMoveIndex = this.index + indexingDirection * numRows * i;
 			if (pieces[possibleMoveIndex] == 0) {
-				this.legalMoves.push([possibleMoveIndex, '']);
+				let move = { moveIndex: possibleMoveIndex, type: '' };
+				if (Math.floor(getRowNum(possibleMoveIndex)) == end) {
+					move[1] = move[1].concat('=');
+				}
+				this.legalMoves.push(move);
 			} else {
 				break;
 			}
 		}
 
-		let oneIndexForward = this.index + indexingDirection * numRows;
-		if (oneIndexForward % numRows >= 0 && oneIndexForward % numRows <= numRows - 2) {
-			let oneIndexForwardRight = oneIndexForward + 1;
-			if (pieces[oneIndexForwardRight] != 0 && pieces[oneIndexForwardRight].isWhite != this.isWhite) {
-				this.legalMoves.push([oneIndexForwardRight, 'x']);
-			}
-		}
-		if (
-			oneIndexForward % numRows == numRows - 1 ||
-			(oneIndexForward % numRows >= 1 && oneIndexForward % numRows <= numRows - 2)
-		) {
-			let oneIndexForwardLeft = oneIndexForward - 1;
-			if (pieces[oneIndexForwardLeft] != 0 && pieces[oneIndexForwardLeft].isWhite != this.isWhite) {
-				this.legalMoves.push([oneIndexForwardLeft, 'x']);
-			}
-		}
-
+		// Check Pawn Captures
 		let leftRightBounds = [
 			[0, numRows - 2, 1],
 			[1, numRows - 1, -1],
 		];
 		for (let i = 0; i < leftRightBounds.length; i++) {
-			if (this.index % numRows >= leftRightBounds[i][0] && this.index % numRows <= leftRightBounds[i][1]) {
+			let oneIndexForward = this.index + indexingDirection * numRows;
+			if (
+				getColNum(oneIndexForward) >= leftRightBounds[i][0] &&
+				getColNum(oneIndexForward) <= leftRightBounds[i][1]
+			) {
+				let oneIndexForwardRightLeft = oneIndexForward + leftRightBounds[i][2];
+				if (pieces[oneIndexForwardRightLeft] != 0 && pieces[oneIndexForwardRightLeft].isWhite != this.isWhite) {
+					let move = { moveIndex: oneIndexForwardRightLeft, type: 'X' };
+					if (Math.floor(oneIndexForwardRightLeft / numRows) == end) {
+						move.type = move.type.concat('=');
+					}
+					this.legalMoves.push(move);
+				}
+			}
+		}
+
+		// Check En Passant
+		for (let i = 0; i < leftRightBounds.length; i++) {
+			if (getColNum(this.index) >= leftRightBounds[i][0] && getColNum(this.index) <= leftRightBounds[i][1]) {
 				let rightLeftIndex = this.index + leftRightBounds[i][2];
 				let rightLeftElement = pieces[rightLeftIndex];
 				if (
@@ -96,21 +110,24 @@ class Pawn extends Piece {
 					rightLeftElement.numMoves == 1 &&
 					moves[moves.length - 1][0] == rightLeftElement
 				) {
-					this.legalMoves.push([rightLeftIndex + indexingDirection * numRows, 'e.p.']);
+					this.legalMoves.push({ moveIndex: rightLeftIndex + indexingDirection * numRows, type: 'e.p.' });
 				}
 			}
 		}
 
-		if (doCheck) {
+		if (runFilterCheck) {
 			this.filterCheck();
 		}
 
 		if (showMoves) {
-			for (let i = 0; i < this.legalMoves.length; i++) {
-				squares[this.legalMoves[i][0]] = '#59b381';
-			}
+			this.showLegalMoves();
 		}
+
 		return this.legalMoves;
+	}
+
+	getIndexingDirection() {
+		return this.isWhite ? -1 : 1;
 	}
 }
 
@@ -130,17 +147,18 @@ class Bishop extends Piece {
 		];
 
 		for (let i = 0; i < diag.length; i++) {
-			let diagIndex = this.index + diag[i][0] * numRows + diag[i][1];
-			while (diagIndex % numRows != diag[i][2] && diagIndex >= 0 && diagIndex < pieces.length) {
+			// Index up/down row and left/right col
+			let diagIndex = this.index + diag[i][1] + diag[i][0] * numRows;
+			while (getColNum(diagIndex) != diag[i][2] && diagIndex >= 0 && diagIndex < pieces.length) {
 				if (pieces[diagIndex] == 0) {
-					this.legalMoves.push([diagIndex, '']);
+					this.legalMoves.push({ moveIndex: diagIndex, type: '' });
 				} else if (pieces[diagIndex].isWhite != this.isWhite) {
-					this.legalMoves.push([diagIndex, 'x']);
+					this.legalMoves.push({ moveIndex: diagIndex, type: 'X' });
 					break;
 				} else {
 					break;
 				}
-				diagIndex += diag[i][0] * numRows + diag[i][1];
+				diagIndex += diag[i][1] + diag[i][0] * numRows;
 			}
 		}
 
@@ -149,10 +167,9 @@ class Bishop extends Piece {
 		}
 
 		if (showMoves) {
-			for (let i = 0; i < this.legalMoves.length; i++) {
-				squares[this.legalMoves[i][0]] = '#59b381';
-			}
+			this.showLegalMoves();
 		}
+
 		return this.legalMoves;
 	}
 }
@@ -176,15 +193,14 @@ class Knight extends Piece {
 			for (let j = 1; j <= 2; j++) {
 				let lMoveIndex = this.index + lMoves[i][0] + lMoves[i][j];
 				if (
-					Math.floor(lMoveIndex / numRows) - Math.floor(this.index / numRows) ==
-						Math.floor(lMoves[i][j] / numRows) &&
+					getRowNum(lMoveIndex) - getRowNum(this.index) == getRowNum(lMoves[i][j]) &&
 					lMoveIndex >= 0 &&
 					lMoveIndex < pieces.length
 				) {
 					if (pieces[lMoveIndex] == 0) {
-						this.legalMoves.push([lMoveIndex, '']);
+						this.legalMoves.push({ moveIndex: lMoveIndex, type: '' });
 					} else if (pieces[lMoveIndex].isWhite != this.isWhite) {
-						this.legalMoves.push([lMoveIndex, 'x']);
+						this.legalMoves.push({ moveIndex: lMoveIndex, type: 'X' });
 					}
 				}
 			}
@@ -195,10 +211,9 @@ class Knight extends Piece {
 		}
 
 		if (showMoves) {
-			for (let i = 0; i < this.legalMoves.length; i++) {
-				squares[this.legalMoves[i][0]] = '#59b381';
-			}
+			this.showLegalMoves();
 		}
+
 		return this.legalMoves;
 	}
 }
@@ -220,11 +235,11 @@ class Rook extends Piece {
 
 		for (let i = 0; i < horzVert.length; i++) {
 			let horzVertIndex = this.index + horzVert[i][0];
-			while (horzVertIndex % numRows != horzVert[i][1] && horzVertIndex >= 0 && horzVertIndex < pieces.length) {
+			while (getColNum(horzVertIndex) != horzVert[i][1] && horzVertIndex >= 0 && horzVertIndex < pieces.length) {
 				if (pieces[horzVertIndex] == 0) {
-					this.legalMoves.push([horzVertIndex, '']);
+					this.legalMoves.push({ moveIndex: horzVertIndex, type: '' });
 				} else if (pieces[horzVertIndex].isWhite != this.isWhite) {
-					this.legalMoves.push([horzVertIndex, 'x']);
+					this.legalMoves.push({ moveIndex: horzVertIndex, type: 'X' });
 					break;
 				} else {
 					break;
@@ -238,10 +253,9 @@ class Rook extends Piece {
 		}
 
 		if (showMoves) {
-			for (let i = 0; i < this.legalMoves.length; i++) {
-				squares[this.legalMoves[i][0]] = '#59b381';
-			}
+			this.showLegalMoves();
 		}
+
 		return this.legalMoves;
 	}
 }
@@ -252,61 +266,18 @@ class Queen extends Piece {
 	}
 
 	getLegalMoves(showMoves, doCheck = false) {
-		this.legalMoves = [];
-
-		let diag = [
-			[1, -1, 7],
-			[1, 1, 0],
-			[-1, -1, 7],
-			[-1, 1, 0],
-		];
-
-		let horzVert = [
-			[-numRows, numRows],
-			[numRows, numRows],
-			[-1, 7],
-			[1, 0],
-		];
-
-		for (let i = 0; i < diag.length; i++) {
-			let diagIndex = this.index + diag[i][0] * numRows + diag[i][1];
-			while (diagIndex % numRows != diag[i][2] && diagIndex >= 0 && diagIndex < pieces.length) {
-				if (pieces[diagIndex] == 0) {
-					this.legalMoves.push([diagIndex, '']);
-				} else if (pieces[diagIndex].isWhite != this.isWhite) {
-					this.legalMoves.push([diagIndex, 'x']);
-					break;
-				} else {
-					break;
-				}
-				diagIndex += diag[i][0] * numRows + diag[i][1];
-			}
-		}
-
-		for (let i = 0; i < horzVert.length; i++) {
-			let horzVertIndex = this.index + horzVert[i][0];
-			while (horzVertIndex % numRows != horzVert[i][1] && horzVertIndex >= 0 && horzVertIndex < pieces.length) {
-				if (pieces[horzVertIndex] == 0) {
-					this.legalMoves.push([horzVertIndex, '']);
-				} else if (pieces[horzVertIndex].isWhite != this.isWhite) {
-					this.legalMoves.push([horzVertIndex, 'x']);
-					break;
-				} else {
-					break;
-				}
-				horzVertIndex += horzVert[i][0];
-			}
-		}
+		let diag = new Bishop(this.index, this.isWhite).getLegalMoves(showMoves, doCheck);
+		let horzVert = new Rook(this.index, this.isWhite).getLegalMoves(showMoves, doCheck);
+		this.legalMoves = diag.concat(horzVert);
 
 		if (doCheck) {
 			this.filterCheck();
 		}
 
 		if (showMoves) {
-			for (let i = 0; i < this.legalMoves.length; i++) {
-				squares[this.legalMoves[i][0]] = '#59b381';
-			}
+			this.showLegalMoves();
 		}
+
 		return this.legalMoves;
 	}
 }
@@ -324,21 +295,22 @@ class King extends Piece {
 			for (let j = -1; j <= 1; j++) {
 				let borderIndex = this.index + possibleMoveIndices[i] + j;
 				if (
-					Math.floor(borderIndex / numRows) == Math.floor((borderIndex - j) / numRows) &&
+					getRowNum(borderIndex) == getRowNum(borderIndex - j) &&
 					borderIndex >= 0 &&
 					borderIndex < pieces.length
 				) {
 					if (pieces[borderIndex] == 0) {
-						this.legalMoves.push([borderIndex, '']);
+						this.legalMoves.push({ moveIndex: borderIndex, type: '' });
 					} else if (pieces[borderIndex].isWhite != this.isWhite) {
-						this.legalMoves.push([borderIndex, 'x']);
+						this.legalMoves.push({ moveIndex: borderIndex, type: 'X' });
 					}
 				}
 			}
 		}
 
+		// Castling
 		let leftRightRookSearch = [1, -1];
-
+		let castlingSpaces = [[], []];
 		if (this.numMoves == 0) {
 			for (let i = 0; i < leftRightRookSearch.length; i++) {
 				let j = this.index + leftRightRookSearch[i];
@@ -356,9 +328,17 @@ class King extends Piece {
 
 				if (pieces[j].constructor.name == 'Rook' && pieces[j].numMoves == 0 && nothingBetween) {
 					let kingQueenSide = leftRightRookSearch[i] > 0 ? '0-0' : '0-0-0';
-					this.legalMoves.push([j - leftRightRookSearch[i], kingQueenSide]);
+					this.legalMoves.push({ moveIndex: j - leftRightRookSearch[i], type: kingQueenSide });
+					if (leftRightRookSearch[i] > 0) {
+						castlingSpaces[0].push({ moveIndex: this.index + leftRightRookSearch[i], type: '' });
+						castlingSpaces[0].push({ moveIndex: j - leftRightRookSearch[i], type: kingQueenSide });
+					}
 					if (leftRightRookSearch[i] < 0) {
-						this.legalMoves.push([j - 2 * leftRightRookSearch[i], kingQueenSide]);
+						this.legalMoves.push({ moveIndex: j - 2 * leftRightRookSearch[i], type: kingQueenSide });
+
+						castlingSpaces[1].push({ moveIndex: j - leftRightRookSearch[i], type: kingQueenSide });
+						castlingSpaces[1].push({ moveIndex: j - 2 * leftRightRookSearch[i], type: kingQueenSide });
+						castlingSpaces[1].push({ moveIndex: this.index + leftRightRookSearch[i], type: '' });
 					}
 				}
 			}
@@ -368,11 +348,31 @@ class King extends Piece {
 			this.filterCheck();
 		}
 
-		if (showMoves) {
-			for (let i = 0; i < this.legalMoves.length; i++) {
-				squares[this.legalMoves[i][0]] = '#59b381';
+		// Tester: setPieces('rnb0kbnrp00ppppp000000000pp00000qPP00000B0N00000P00PPPPPR000KBNRm')
+		// Tester: setPieces('rnb0kbnrp00p000p000000000pp0p000qPP0000PB0N00N0BP00PP000R000K00R')
+		// Tester: setPieces('rnb0kbr0p00pn00p000000000pp0p000qPPPP00PB0N00N0BP0000000R000K00R')
+		// If the king can't go to spaces between Rook and King because of checking, it cant castle
+		for (let i = 0; i < castlingSpaces.length; i++) {
+			let containsAllCastlingSide = true;
+			for (let j = 0; j < castlingSpaces[i].length; j++) {
+				if (findsLegalMoves(this.legalMoves, castlingSpaces[i][j][0]) == -1) {
+					containsAllCastlingSide = false;
+					break;
+				}
+			}
+			if (!containsAllCastlingSide) {
+				for (let j = 0; j < castlingSpaces[i].length; j++) {
+					this.legalMoves = this.legalMoves.filter((value) => {
+						return value[0] != castlingSpaces[i][j][0] || !castlingSpaces[i][j][1].includes('0');
+					});
+				}
 			}
 		}
+
+		if (showMoves) {
+			this.showLegalMoves();
+		}
+
 		return this.legalMoves;
 	}
 }
