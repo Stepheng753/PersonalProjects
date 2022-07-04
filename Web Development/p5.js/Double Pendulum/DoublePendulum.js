8a81fcfad2c55a55b813c4c879f2058b687a83dc
Stephen Giang <stepheng753@gmail.com>
2022-07-04 13:39:13 -0700

    Made changes to add hanging mass1

--- Web Development/p5.js/Double Pendulum/DoublePendulum.js
+++ Web Development/p5.js/Double Pendulum/DoublePendulum.js
@@ -6,8 +6,12 @@ let dtheta2;
 let start;
 let prevPts;
 
+let g = 1;
+let m1 = 10;
+let m2 = 10;
 let l1;
 let l2;
+
 let cx;
 let cy;
 let buffer;
@@ -16,9 +20,6 @@ let py2;
 
 const frameSpeed = 1;
 const frameDuration = 2000;
-const g = 1;
-const m1 = 10;
-const m2 = 10;
 
 function setup() {
 	createCanvas(canvasWidth * 0.9, canvasWidth * 0.9);
@@ -26,8 +27,6 @@ function setup() {
 	angleMode(RADIANS);
 	l1 = height / 3;
 	l2 = height / 8;
-	// l1 = 125;
-	// l2 = 125;
 	cx = width / 2;
 	cy = height / 2;
 
@@ -46,11 +45,12 @@ function draw() {
 	let y2 = y1 + l2 * cos(theta2);
 
 	if (start > 0) {
-		d2theta = calcd2theta();
+		let d2theta = calcd2theta();
 		dtheta1 += d2theta[0];
 		dtheta2 += d2theta[1];
 		theta1 += dtheta1;
 		theta2 += dtheta2;
+		console.log(theta1, theta2, dtheta1, dtheta2, d2theta[0], d2theta[1]);
 
 		if (frameCount % frameSpeed == 0) {
 			prevPts.push([x2, y2]);
@@ -99,7 +99,7 @@ function calcd2theta() {
 
 	d2theta2 = numerator2 / denominator2;
 
-	return [d2theta1, d2theta2];
+	return [max(-10, min([d2theta1, 10])), max(-10, min([d2theta2, 10]))];
 }
 
 function drawPendulum(x1, y1, x2, y2) {
@@ -120,22 +120,52 @@ function drawPrevPts() {
 }
 
 function drawTrace(x2, y2) {
-	if (px2) {
+	if (px2 && start > 0) {
 		buffer.strokeWeight(2);
 		buffer.stroke((frameCount - start) % 360, 100, 100);
 		buffer.line(px2, py2, x2, y2);
 	}
 }
 
-function mousePressed() {
-	let theta = atan2(cy - mouseY, mouseX - cx) + Math.PI / 2;
-	theta1 = theta;
-	theta2 = theta;
-	initSim(frameCount);
-	console.log(theta1, theta2);
+function mouseDragged() {
+	if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+		initSim(0);
+		angleMode(RADIANS);
+		let x = mouseX - cx;
+		let y = mouseY - cy;
+		let theta = -atan2(y, x) + Math.PI / 2;
+		let d = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+		console.log(d, l1 + l2);
+		if (d >= l1 + l2 || d <= l1 - l2) {
+			theta1 = theta;
+			theta2 = theta;
+			console.log(mouseX);
+		} else {
+			innerTheta = acos((Math.pow(l2, 2) - Math.pow(d, 2) - Math.pow(l1, 2)) / (-2 * d * l1));
+			if (x < 0) {
+				innerTheta *= -1;
+			}
+			theta1 = theta - innerTheta;
+			let x1 = l1 * sin(theta1);
+			let y1 = l1 * cos(theta1);
+			let dx = -x1 + x;
+			let dy = y1 - y;
+			theta2 = atan2(dy, dx) + Math.PI / 2;
+		}
+	}
+	return false;
+}
+
+function mouseReleased() {
+	if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+		loop();
+		initSim(frameCount);
+	}
 }
 
 function initSim(startCt) {
+	loop();
 	start = startCt;
 	prevPts = [];
 	dtheta1 = 0;
@@ -146,4 +176,47 @@ function initSim(startCt) {
 	buffer.background(0);
 	buffer.colorMode(HSB, 360, 100, 100);
 	buffer.translate(cx, cy);
+}
+
+window.onload = () => {
+	let container = document.getElementById('container');
+	let length1Label = document.createElement('label');
+	let length1Input = document.createElement('input');
+	length1Input.id = 'length1';
+	length1Input.type = 'number';
+	length1Input.value = 260;
+	length1Input.step = 10;
+	length1Input.min = 100;
+	length1Input.max = height / 3;
+	length1Input.onchange = () => updateParameters();
+	length1Label.style = 'padding-left: 15px';
+	length1Label.innerHTML = 'Length 1';
+	container.append(length1Label);
+	container.appendChild(length1Input);
+	let length2Label = document.createElement('label');
+	let length2Input = document.createElement('input');
+	length2Input.id = 'length2';
+	length2Input.type = 'number';
+	length2Input.value = 100;
+	length2Input.step = 5;
+	length2Input.min = 10;
+	length2Input.max = width / 2 - length1Input.value;
+	length2Input.onchange = () => updateParameters();
+	length2Label.style = 'padding-left: 15px';
+	length2Label.innerHTML = 'Length 2';
+	container.append(length2Label);
+	container.appendChild(length2Input);
+	console.log(length2Input);
+};
+
+function updateParameters() {
+	let length1Input = document.getElementById('length1');
+	let length2Input = document.getElementById('length2');
+	length2Input.max = width / 2 - length1Input.value;
+
+	g = float(document.getElementById('gravity').value);
+	m1 = float(document.getElementById('mass1').value);
+	m2 = float(document.getElementById('mass2').value);
+	l1 = float(document.getElementById('length1').value);
+	l2 = float(document.getElementById('length2').value);
 }
